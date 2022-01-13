f1990a0827da127f0f9622f3a821597362e59b9d
Joshua Rose <universalnerd@gmail.com>
2022-01-13 14:32:26 -0800

    Simplify card up/down logic, add flip function.

--- Card.gd
+++ Card.gd
@@ -2,6 +2,8 @@ extends TextureButton
 
 class_name Card
 
+signal card_clicked
+
 # Suit numbers included as constants for reference/convenience
 const HEARTS = 1
 const CLUBS = 2
@@ -12,8 +14,6 @@ var suit	# Hearts, Clubs, Spades, Diamonds
 var value	# 1-13, e.g. 1 = Ace, 11 = Jack, etc.
 var face	# Image resource to display on front of card
 var back	# Image resource to display on back of card
-
-var faceup:bool setget set_face_up
 
 func _ready():
 	set_h_size_flags(3)
@@ -28,10 +28,11 @@ func _init(var st, var val):
 	self.back = GameController.cardBack
 	set_normal_texture(self.face)
 
-func set_face_up(var face):
-	if face != faceup:
-		self.flip()
-	faceup = face
+func _pressed():
+	emit_signal("card_clicked", self)
 
 func flip():
-	pass
+	if get_normal_texture() == self.back:
+		set_normal_texture(self.face)
+	else:
+		set_normal_texture(self.back)
